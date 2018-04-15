2d0fcf0a7138ab6bf8096fe5ccf5601d1eefedc0
Seanna <1403025820@qq.com>
2018-04-15 23:35:08 +0800

    Dom编程艺术第三章完

--- day3/scripts/showPic.js
+++ day3/scripts/showPic.js
@@ -9,17 +9,19 @@ function showPic(whichPic) {
     var source = whichPic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
-
     //实现文本切换
     var text=whichPic.getAttribute("title");
-    var description=whichPic.getElementById("description");
-    alert(description.nodeValue);
+    var description=document.getElementById("description");
+    description.firstChild.nodeValue=text;
+    //alert(description.nodeValue);//null
+    //alert(description.childNodes[0].nodeValue);//返回文本节点
+    //alert(description.firstChild.nodeValue);//firstChild===childNodes[0]
 }
 
 //页面加载时调用的函数
-window.onload=function countBodyChilddren(){
-    var body_element=document.getElementsByTagName("body")[0];
-    //alert(body_element.length);
-    alert(body_element.childNodes.length);
-}
+//window.onload=function countBodyChilddren(){
+//    var body_element=document.getElementsByTagName("body")[0];
+//    alert(body_element.length);
+//    alert(body_element.childNodes.length);
+//}
 
